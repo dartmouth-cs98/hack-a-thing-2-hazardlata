486731b0b75546f28ccad2ef87b5ef340351d36b
Max Lata <maxwell.a.lata.20@dartmouth.edu>
2019-09-28 16:35:23 -0400

    Adding logic for food

--- snake.py
+++ snake.py
@@ -15,18 +15,19 @@ class Snake:
         self.left = (-1, 0)
         self.up = (0, 1)
         self.down = (0, -1)
+        self.max_food_tries = 100
 
         # snake components
-        self.snake = []                 # array of snake (x, y) coordinates for each block of the snake
-        self.direction = (0, 1)         # x, y where (0, 1) is up, (0, -1) is down, (1, 0) is right, and (0, 1) is left
+        self.snake = []                             # array of snake (x, y) coordinates for each block of the snake
+        self.direction = (0, 1)                     # x, y where (0, 1) is up, (0, -1) is down, (1, 0) is right, and (0, 1) is left
         self.score = 0
-        self.food = None
+        self.food = self.spawn_new_food()
 
     #### GAME LOGIC ####
+    # checks for collisions
     def check_collision(self):
         # get coordinates of snake head
-        head = self.snake[0]
-        head_x, heady_y = head[0], head[1]
+        head_x, heady_y = self.snake[0]
 
         # checks out-of-bounds snake (no-wrapping around screen)
         if head_x < 0 or head_x > self.width or head_y < 0 or head_y > self.height:
@@ -39,6 +40,51 @@ class Snake:
 
         # no collision
         return False
+
+    # generates random location for food
+    def generate_random_coordinate(self):
+        rand_x = randint(0, self.width)
+        rand_y = randint(0, self.height)
+
+        return (rand_x, rand_y)
+
+    # checks if food is on snake
+    def food_on_snake(self, food_x, food_y):
+        for each in self.snake:
+            if (food_x, food_y) == each:        # loop and check collisions
+                return True
+
+        return False
+
+    # spawns new food, not on snake
+    def spawn_new_food(self):
+        # generates until a food location not on the snake is found (within reason)
+        while True:
+            food_x, food_y = self.generate_random_coordinate()
+
+            if not food_on_snake(food_x, food_y):
+                return (food_x, food_y)
+
+    # determines if snake has eaten food
+    def snake_ate(self):
+        return self.snake[0] == self.food
+
+    # updates snake
+    def update(self):
+        # if the snake ate, elongate him/her and spawn new food
+        if self.snake_ate():
+            self.snake.insert(0, self.food)
+            self.food = self.spawn_new_food()
+        # if not,
+        else:
+            # calculate where new head will be
+            new_head_coordinates = self.add_vectors(self.snake[0], self.direction)
+
+            # insert new head_coordinates into beginning of list
+            self.snake.insert(0, new_head_coordinates)
+
+            # pop tail
+            self.snake.pop()
 
     #### INPUTS ####
     def turn_right(self):
@@ -54,16 +100,6 @@ class Snake:
         self.direction = self.down
 
     #### GRAPHICS ####
-    # updates graphics
-    def update(self):
-        # calculate where new head will be
-        new_head_coordinates = self.add_vectors(self.snake[0], self.direction)
-
-        # insert new head_coordinates into beginning of list
-        self.snake.insert(0, new_head_coordinates)
-
-        # pop tail
-        self.snake.pop()
 
     # adds vectors for easier snake drawing
     def add_vectors(self, (x1, y1), (x2, y2)):
@@ -76,6 +112,7 @@ class Snake:
 
     # draws everything (wrapped for passing to CS1lib)
     def draw_all(self):
+        # TODO - draw food first then snake so snake is always over food
         self.draw_board()
 
     # displays everything
