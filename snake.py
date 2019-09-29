cda2a4e3d8772d17610716a0c567163cb4607a49
Max Lata <maxwell.a.lata.20@dartmouth.edu>
2019-09-29 17:13:32 -0400

    Additional collision and food logic; print out score at end

--- snake.py
+++ snake.py
@@ -1,25 +1,28 @@
 from cs1lib import *
 from random import randint
+from time import sleep
 
 class Snake:
-    def __init__(self):
-        # graphical components
-        self.board_width = 1600
-        self.board_height = 1600
-        self.snake_block_dimensions = 10
-
+    def __init__(self, width, height, p_width, p_height):
         # game components
-        self.width = 20
-        self.height = 20
+        self.width = width
+        self.height = height
         self.right = (1, 0)
         self.left = (-1, 0)
-        self.up = (0, 1)
-        self.down = (0, -1)
+        self.up = (0, -1)
+        self.down = (0, 1)
         self.max_food_tries = 100
+        self.start = False
+        self.end = False
+
+        # graphical components
+        self.board_width = p_width
+        self.board_height = p_height
+        self.snake_block_dimensions = p_width / width
 
         # snake components
-        self.snake = []                             # array of snake (x, y) coordinates for each block of the snake
-        self.direction = (0, 1)                     # x, y where (0, 1) is up, (0, -1) is down, (1, 0) is right, and (0, 1) is left
+        self.snake = [self.generate_random_coordinate()]                                # array of snake (x, y) coordinates for each block of the snake
+        self.direction = None                                                       # x, y where (0, 1) is up, (0, -1) is down, (1, 0) is right, and (0, 1) is left
         self.score = 0
         self.food = self.spawn_new_food()
 
@@ -27,15 +30,15 @@ class Snake:
     # checks for collisions
     def check_collision(self):
         # get coordinates of snake head
-        head_x, heady_y = self.snake[0]
+        head_x, head_y = self.snake[0]
 
         # checks out-of-bounds snake (no-wrapping around screen)
-        if head_x < 0 or head_x > self.width or head_y < 0 or head_y > self.height:
+        if head_x < -1 or head_x >= self.width or head_y < -1 or head_y >= self.height:
             return True
 
         # checks self-colliding snake
-        for each in self.snake[1:]
-            if each = (head_x, head_y):
+        for each in self.snake[1:]:
+            if each == (head_x, head_y):
                 return True
 
         # no collision
@@ -43,8 +46,8 @@ class Snake:
 
     # generates random location for food
     def generate_random_coordinate(self):
-        rand_x = randint(0, self.width)
-        rand_y = randint(0, self.height)
+        rand_x = randint(0, self.width - 1)
+        rand_y = randint(0, self.height - 1)
 
         return (rand_x, rand_y)
 
@@ -59,10 +62,10 @@ class Snake:
     # spawns new food, not on snake
     def spawn_new_food(self):
         # generates until a food location not on the snake is found (within reason)
-        while True:
+        for i in range(self.max_food_tries):
             food_x, food_y = self.generate_random_coordinate()
 
-            if not food_on_snake(food_x, food_y):
+            if not self.food_on_snake(food_x, food_y):
                 return (food_x, food_y)
 
     # determines if snake has eaten food
@@ -73,9 +76,10 @@ class Snake:
     def update(self):
         # if the snake ate, elongate him/her and spawn new food
         if self.snake_ate():
+            self.score += 1
             self.snake.insert(0, self.food)
             self.food = self.spawn_new_food()
-        # if not,
+        # if not, just move the snake around
         else:
             # calculate where new head will be
             new_head_coordinates = self.add_vectors(self.snake[0], self.direction)
@@ -86,38 +90,101 @@ class Snake:
             # pop tail
             self.snake.pop()
 
+            if self.check_collision():
+                print("Collision")
+                print(self.snake[0])
+
+                set_clear_color(1, 1, 1)
+                clear()
+                set_stroke_color(0, 0, 0)
+                set_font_size(100)
+                draw_text(str(self.score), self.board_width // 2, self.board_height // 2)
+                self.start = False
+                self.end = True
+
+    #### USER INPUT FOR TESTING ####
+    def pressed(self, key):
+        self.start = True
+        if key == "a":
+            self.turn_left()
+        elif key == "d":
+            self.turn_right()
+        elif key == "s":
+            self.turn_down()
+        elif key == "w":
+            self.turn_up()
+
     #### INPUTS ####
     def turn_right(self):
-        self.direction = self.right
+        if self.direction != self.left:
+            print("Turning right")
+            self.direction = self.right
 
     def turn_left(self):
-        self.direction = self.left
+        if self.direction != self.right:
+            print("Turning left")
+            self.direction = self.left
 
     def turn_up(self):
-        self.direction = self.up
+        if self.direction != self.down:
+            print("Turning up")
+            self.direction = self.up
 
-    def turn_down(self):
-        self.direction = self.down
+    def turn_down(self):        # for what
+        if self.direction != self.up:
+            print("Turning down")
+            self.direction = self.down
 
     #### GRAPHICS ####
-
     # adds vectors for easier snake drawing
-    def add_vectors(self, (x1, y1), (x2, y2)):
+    def add_vectors(self, v1, v2):
+        x1, y1 = v1
+        x2, y2 = v2
         return (x1 + x2, y1 + y2)
 
-    # draws the board itself
-    def draw_board(self):
-        set_fill_color(0, 0, 0)
-        draw_rectangle(0, 0, self.board_width, self.board_height)
+    # draws the food
+    def draw_food(self):
+        set_fill_color(1, 1, 0)
+        self.draw_block(self.food)
+
+    # draws a block (either snake or food)
+    def draw_block(self, coord):
+        # get graphical coordinates
+        graphics_x, graphics_y = coord
+        graphics_x *= self.snake_block_dimensions
+        graphics_y *= self.snake_block_dimensions
+
+        # draw the block
+        draw_rectangle(graphics_x, graphics_y, self.snake_block_dimensions, self.snake_block_dimensions)
+
+    # draws the snake itself
+    def draw_snake(self):
+        # draw head in red
+        set_fill_color(1, 0, 0)
+        self.draw_block(self.snake[0])
+
+        # draw body in green
+        set_fill_color(0, 1, 0)
+        for each in self.snake[1:]:
+            self.draw_block(each)
 
     # draws everything (wrapped for passing to CS1lib)
-    def draw_all(self):
-        # TODO - draw food first then snake so snake is always over food
-        self.draw_board()
+    def play(self):
+        if not self.end:
+            set_clear_color(0, 0, 0)
+            clear()
+
+            # TODO - draw food first then snake so snake is always over food
+            self.draw_food()
+            self.draw_snake()
+
+            # only updates once game is ready to be played
+            if self.start:
+                self.update()
 
     # displays everything
     def display(self):
-        start_graphics(self.draw_all, width=self.board_width, height=self.board_height)
+        start_graphics(self.play, framerate=10, width=self.board_width, height=self.board_height, key_press=self.pressed)
 
-snake = Snake()
+snake = Snake(20, 20, 2000, 2000)
 snake.display()
